10b67dfa0185ef005ba7c339ff2d786884deda94
Your Name <ozjuanpa@gmail.com>
2025-02-26 23:20:52 -0500

    messed up offset

--- Tokenize.hs
+++ Tokenize.hs
@@ -4,39 +4,41 @@ import ValidWords (conceptMap, FeaturedWord(..))
 import qualified Data.Map as Map
 
 data Token =
-    PlainToken String
-    | PunctuationToken String
+    PlainToken String Int Int
+    | PunctuationToken String Int Int
     deriving (Show, Eq)
 
 data TokenizationState = TokenizationState {
     buffer :: String,
     tokens :: [Token],
-    parsedChars :: Int
+    parsedChars :: Int,
+    line :: Int,
+    column :: Int
 } deriving (Show, Eq)
 
 --tokenize :: String -> [Token]
 
 commitFromPunctuation :: TokenizationState -> [ Token ] -> TokenizationState
-commitFromPunctuation (TokenizationState buffer tokens u) extraTokens =
-    TokenizationState "" (tokens ++ [PlainToken buffer] ++ extraTokens) (u + 1)
+commitFromPunctuation (TokenizationState buffer tokens u l o) extraTokens =
+    TokenizationState "" (tokens ++ [PlainToken buffer l o] ++ extraTokens) (u + 1) l (o + 1)
 
 commitFromSpace :: TokenizationState -> TokenizationState
-commitFromSpace (TokenizationState buffer tokens u) 
-    | null buffer = TokenizationState "" tokens (u + 1)
-    | otherwise = TokenizationState "" (tokens ++ [PlainToken buffer]) (u + 1)
+commitFromSpace (TokenizationState buffer tokens u l o) 
+    | null buffer = TokenizationState "" tokens (u + 1) l (o + 1)
+    | otherwise = TokenizationState "" (tokens ++ [PlainToken buffer l (o - (length buffer))]) (u + 1) l (o + 1)
 
 commitFromNewline :: TokenizationState -> TokenizationState
-commitFromNewline (TokenizationState buffer tokens u) 
-    = TokenizationState "" (tokens ++ [PunctuationToken "\n"]) (u + 1)
+commitFromNewline (TokenizationState buffer tokens u l o) 
+    = TokenizationState "" (tokens ++ [PunctuationToken "\n" l o]) (u + 1) (l + 1) 1
 
 addCharacter :: TokenizationState -> Char -> TokenizationState
-addCharacter (TokenizationState buffer tokens u) c 
-    = TokenizationState (buffer ++ [c]) tokens (u + 1)
+addCharacter (TokenizationState buffer tokens u l o) c 
+    = TokenizationState (buffer ++ [c]) tokens (u + 1) l (o + 1)
 
 tokenizeInto :: TokenizationState -> Char -> TokenizationState
-tokenizeInto state@(TokenizationState buffer tokens u) c
+tokenizeInto state@(TokenizationState buffer tokens u l o) c
     | c `elem` ['.', ':', ','] 
-        = commitFromPunctuation state [ PunctuationToken [c] ]
+        = commitFromPunctuation state [ PunctuationToken [c] l o ]
     | c `elem` [' ', '\t'] = commitFromSpace state
     | c == '\n' = 
         if null buffer
@@ -54,12 +56,20 @@ grabFeaturedWord word =
         Nothing -> error ("Unknown word: >" ++ word ++ "<")
 
 emitAffixes :: Token -> [Token]
-emitAffixes (PlainToken value) = 
+emitAffixes (PlainToken value l o) = 
     let (FeaturedWord word prefix suffix sufsuffix) = grabFeaturedWord value
-        prefixToken = maybe [] (\x -> [ PlainToken x ]) prefix
-        suffixToken = maybe [] (\x -> [ PlainToken x ]) suffix
-        sufsuffixToken = maybe [] (\x -> [ PlainToken x ]) sufsuffix
-    in prefixToken ++ [ PlainToken word ] ++ suffixToken ++ sufsuffixToken
+        prefixLength = maybe 0 length prefix
+        suffixLength = maybe 0 length suffix
+        sufsuffixLength = maybe 0 length sufsuffix
+        wordLength = length word
+        sufsuffixOffset = o - sufsuffixLength
+        suffixOffset = sufsuffixOffset - suffixLength
+        wordOffset = suffixOffset - wordLength
+        prefixOffset = wordOffset - prefixLength
+        prefixToken = maybe [] (\x -> [ PlainToken x l (o - prefixOffset) ]) prefix
+        suffixToken = maybe [] (\x -> [ PlainToken x l (o - suffixOffset) ]) suffix
+        sufsuffixToken = maybe [] (\x -> [ PlainToken x l (o - sufsuffixOffset) ]) sufsuffix
+    in prefixToken ++ [ PlainToken word l (o - wordLength) ] ++ suffixToken ++ sufsuffixToken
 
 emitAffixes token = [ token ]
 
@@ -69,11 +79,10 @@ withAffixes tokens = concatMap emitAffixes tokens
 validate :: [Token] -> [Token]
 validate tokens = map check tokens
     where 
-        check (PlainToken "") = error "Empty plain token"
-        check (PunctuationToken "") = error "Empty punctuation token"
-        check (PlainToken value) = PlainToken value
-        check (PunctuationToken value) = PunctuationToken value
+        check (PlainToken "" l o) = error ("Empty plain token from line " ++ show l ++ " at offset " ++ show o)
+        check (PunctuationToken "" l o) = error ("Empty punctuation token from line" ++ show l ++ " at offset " ++ show o)
+        check token = token
 
 tokenizeString :: String -> [Token]
-tokenizeString str = validate $ withAffixes $ tokens $ tokenizeStringInto (TokenizationState "" [] 0) str
+tokenizeString str = validate $ withAffixes $ tokens $ tokenizeStringInto (TokenizationState "" [] 0 1 1) str
 
